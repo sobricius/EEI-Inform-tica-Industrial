52d66d53ce58fab583cff9b5b9122f19485703ea
Sergio Sobrido <sobricius@gmail.com>
2024-09-11 00:51:49 +0200

    Prueba

--- EEI Informatica Industrial/main.py
+++ EEI Informatica Industrial/main.py
@@ -1,7 +1,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hello World, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello Worldiooo, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
